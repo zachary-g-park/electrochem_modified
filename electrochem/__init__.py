be4c9c36be1b65cb154c1720bf28b6dbf316dcbc
zachary-g-park <zachary.g.park@gmail.com>
2024-05-29 17:41:42 -0400

    Update __init__.py

--- electrochem/__init__.py
+++ electrochem/__init__.py
@@ -1,9 +1,10 @@
 """Top-level package for Arbin Electrochemical Tools."""
-import electrochem._version
+from electrochem._version import __version__
 
 __author__ = """Vincent Wu"""
 __email__ = 'vincentwu@ucsb.edu'
-__version__ = __version__ = electrochem._version.__version__
+__version__ = __version__ 
+
 from electrochem.parse import (parseArbin, toDataframe, extractEchem, 
 extractCycleEchem, generateSummary, generateEchemSummary, plotEchem)
 
